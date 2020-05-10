45f1ba6b7f55e3e45f20048dc775692ee58d5e93
Leon Harper <leon.harper@gmail.com>
2020-05-10 20:28:15 +0100

    for loop to create a triangle

---
+++ triangleLoop.js
@@ -0,0 +1,7 @@
+const endTriangle = 6;
+let outputString = '#';
+
+for (let currentTriangle = 0; currentTriangle <= endTriangle; currentTriangle++) {
+    console.log(outputString);
+    outputString += '#';
+}
